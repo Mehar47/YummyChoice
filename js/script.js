8584e923711ef666ab98de06b1eb13e6f0544f21
mehar singh <meharsinghgabria@gmail.com>
2025-05-15 21:04:22 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// script.js
+const nav = document.querySelector('nav');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
